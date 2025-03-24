9a8cc10554bc4a8cf1142dc9e4b974d63d589212
kudouyuki209 <k024g0023@g.neec.ac.jp>
2025-03-24 15:29:44 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow(L"LE2D_01_クドウ_ユウキ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
